19614ba7d6364822ac7037930bc772d7da15fe98
Felis-Catu <felicitymalong@gmail.com>
2025-11-02 17:39:22 -0600

    installed table packages

--- dashboard/src/app/longterm/housekeeping/maintenance/page.tsx
+++ dashboard/src/app/longterm/housekeeping/maintenance/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 
 import FilterButton from "../../../../components/FilterButton";
+import {Input} from "@/components/table/input";
 
 export default function Home() {
 
 
   return (
     <div className =" ">
-
+        <Input/>
 
     </div>
 
